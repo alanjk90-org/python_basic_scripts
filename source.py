fbf36da22fa6cde071c103819689d4ac7128250d
alanjk90 <alanjk90@gmail.com>
2020-04-22 23:16:49 +0530

    remove all file and created test file

--- source.py
+++
@@ -1,8 +0,0 @@
-if __name__== "__main__":
-    print type(__name__)
-    print "__name__ is ", __name__
-   # print __main__
-
-print """ __name__ will be "__main__" if we execute the script like ./script file
-and if it it is not executing but imported, __name__ will not be __main__
-         """
